2738d846df4e313c0d25b7a835df9dfd7ff68134
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-04-24 16:12:29 +0200

    deps updated

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.0.5"]
-                 [metosin/schema-tools "0.7.0"]
+                 [prismatic/schema "1.1.0"]
+                 [metosin/schema-tools "0.9.0"]
                  ]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
