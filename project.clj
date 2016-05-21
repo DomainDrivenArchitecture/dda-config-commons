d0331501cbc1195c426c1573947fdbf836ef741e
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-05-21 11:57:45 +0200

    lein-release plugin: bumped version from 0.1.4 to 0.1.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-config-commons "0.1.4"
+(defproject org.domaindrivenarchitecture/dda-config-commons "0.1.5-SNAPSHOT"
   :description "common utils for dda config"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
