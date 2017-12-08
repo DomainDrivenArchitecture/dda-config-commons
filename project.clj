aae25a7dded8fd423948eb9228a1d3235aff5092
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-12-08 19:12:19 +0100

    Refactoring: moved secret to config-commons

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-config-commons "0.2.4-SNAPSHOT"
+(defproject dda/dda-config-commons "0.3.0-SNAPSHOT"
   :description "common utils for dda config"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
