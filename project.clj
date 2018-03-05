fe9c4b430fed756909e2039e5961c38199837198
jem <michael.jerger@meissa-gmbh.de>
2018-03-05 19:02:33 +0100

    going to release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/schema "1.1.7"]
-                 [metosin/schema-tools "0.9.1"]
+                 [metosin/schema-tools "0.10.0"]
                  [ch.qos.logback/logback-classic "1.3.0-alpha0"]
                  [commons-codec "1.11"]]
   :source-paths ["main/src"]
