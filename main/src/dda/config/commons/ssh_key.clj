a7d83e7c877478da0ffd9154d4d8aead0abcb4da
jem <michael.jerger@meissa-gmbh.de>
2018-03-05 10:59:30 +0100

    refactor usser-env to ssh

--- main/src/dda/config/commons/ssh_key.clj
+++ main/src/dda/config/commons/ssh_key.clj
@@ -13,6 +13,7 @@
 ; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
+
 (ns dda.config.commons.ssh-key
   (:require
    [schema.core :as s]))
@@ -27,3 +28,66 @@
 (def SshKeyPair
   {:public-key PublicSshKey
    :private-key PrivateSshKey})
+
+(s/defn ^:always-validate
+  string-to-pub-key-config :- PublicSshKey
+  [pub-key :- s/Str]
+  "function takes a public-key as a string and returns it as a ssh-public-key-config"
+  (let [col (clojure.string/split pub-key #" ")]
+    {:type (first col)
+     :public-key (second col)
+     :comment (nth col 2)}))
+
+(s/defn ^:always-validate
+  format-public-key :- s/Str
+  "returns a formatted public-key from an ssh-config"
+  [ssh-public-key-config :- PublicSshKey]
+  (str
+   (:type ssh-public-key-config) " "
+   (:public-key ssh-public-key-config) " "
+   (:comment ssh-public-key-config)))
+
+(s/defn
+  ssh-priv-key-from-env-to-config :- PrivateSshKey
+ "function reads ssh private key from environment variable and returns it as a String"
+ []
+ (let [env-variable "SSH_PRIV_KEY"]
+   (System/getenv env-variable)))
+
+(defn user-home-dir
+  "provides the user home path."
+  [user-name]
+  (if (= user-name "root")
+    "/root"
+    (str "/home/" user-name)))
+
+(defn user-ssh-dir
+ "provides the user .ssh path."
+ [user-name]
+ (str (user-home-dir user-name) "/.ssh/"))
+
+(s/defn ssh-priv-key-from-env-to-config :- PrivateSshKey
+ "function reads ssh private key from environment variable and returns it as a String"
+ []
+ (let [env-variable "SSH_PRIV_KEY"]
+   (System/getenv env-variable)))
+
+(defn read-ssh-pub-key-to-config
+ "read the ssh-public-key to a config"
+ [& {:keys [ssh-dir-path]}]
+ (let [ssh-dir (or ssh-dir-path (str (System/getenv "HOME") "/.ssh"))]
+   (string-to-pub-key-config (slurp (str ssh-dir "/id_rsa.pub")))))
+
+(defn read-ssh-priv-key-to-config
+ "read the ssh-private-key to a config"
+ [& {:keys [ssh-dir-path read-from-env?]}]
+ (let [ssh-dir (or ssh-dir-path (str (System/getenv "HOME") "/.ssh"))]
+   (if read-from-env? (ssh-priv-key-from-env-to-config) (slurp (str ssh-dir "/id_rsa")))))
+
+(defn read-ssh-keys-to-pair-config
+ [& {:keys [ssh-dir-path read-from-env?]}]
+ "read ssh-keys from current node to ssh-key-pair-config. If read-from-env? flag is specified,
+  ssh-private-key will be read from enviroment variable SSH_PRIV_KEY"
+ {:public-key (read-ssh-pub-key-to-config :ssh-dir-path ssh-dir-path)
+  :private-key (read-ssh-priv-key-to-config :ssh-dir-path ssh-dir-path
+                                            :read-from-env? read-from-env?)})
