cafa564ad2564d2645e648564a8113b7b63b4b81
jem <michael.jerger@meissa-gmbh.de>
2019-04-24 15:04:37 +0200

    added sublic & update deps

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [prismatic/schema "1.1.9"]
-                 [metosin/schema-tools "0.10.3"]
-                 [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                 [commons-codec "1.11"]]
+                 [prismatic/schema "1.1.10"]
+                 [metosin/schema-tools "0.11.0"]
+                 [ch.qos.logback/logback-classic "1.3.0-alpha4" :exclusions [com.sun.mail/javax.mail]]
+                 [commons-codec "1.12"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -20,9 +20,9 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha3"]
+                   [[org.clojure/test.check "0.10.0-alpha4"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]
                    :plugins
                    [[lein-sub "0.3.0"]]
                    :leiningen/reply
